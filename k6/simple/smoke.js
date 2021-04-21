a7346a169c6c07e524a227e2901bca26e1ceab57
nokchax <nokchax@gmail.com>
2021-04-21 21:31:26 +0900

    feat: add k6 local test script

---
+++ k6/simple/smoke.js
@@ -0,0 +1,36 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export let options = {
+    vus: 1, // 1 user looping for 1 minute
+    duration: '10s',
+
+    thresholds: {
+        http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    },
+};
+
+const BASE_URL = 'http://localhost:8080';
+const USERNAME = 'probitanima11@gmail.com';
+const PASSWORD = '11';
+
+export default () => {
+    let loginRes = http.post(`${BASE_URL}/login/token`, {
+        email: USERNAME,
+        password: PASSWORD,
+    });
+
+    check(loginRes, {
+        'logged in successfully': (resp) => resp.json('accessToken') !== '',
+    });
+
+
+    let authHeaders = {
+        headers: {
+            Authorization: `Bearer ${loginRes.json('accessToken')}`,
+        },
+    };
+    let myObjects = http.get(`${BASE_URL}/members/me`, authHeaders).json();
+    check(myObjects, { 'retrieved member': (obj) => obj.id != 0 });
+    sleep(1);
+};
---
+++ k6/simple/stress.js
@@ -0,0 +1,38 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '1m', target: 2000 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+        { duration: '10s', target: 0 }, // ramp-down to 0 users
+    ],
+    thresholds: {
+        http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+        'logged in successfully': ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    },
+};
+
+const BASE_URL = 'http://localhost:8080';
+const USERNAME = 'probitanima11@gmail.com';
+const PASSWORD = '11';
+
+export default () => {
+    let loginRes = http.post(`${BASE_URL}/login/token`, {
+        email: USERNAME,
+        password: PASSWORD,
+    });
+
+    check(loginRes, {
+        'logged in successfully': (resp) => resp.json('accessToken') !== '',
+    });
+
+
+    let authHeaders = {
+        headers: {
+            Authorization: `Bearer ${loginRes.json('accessToken')}`,
+        },
+    };
+    let myObjects = http.get(`${BASE_URL}/members/me`, authHeaders).json();
+    check(myObjects, { 'retrieved member': (obj) => obj.id != 0 });
+    sleep(1);
+};
