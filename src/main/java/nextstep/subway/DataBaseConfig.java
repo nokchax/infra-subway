2ca13a7ccc4dab7c552316c2e9a01157e5dc62fe
nokchax <nokchax@gmail.com>
2021-04-23 18:46:24 +0900

    rollback

--- src/main/java/nextstep/subway/DataBaseConfig.java
+++ src/main/java/nextstep/subway/DataBaseConfig.java
@@ -17,46 +17,46 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.sql.DataSource;
 import java.util.HashMap;
 
-import static nextstep.subway.ReplicationRoutingDataSource.DATASOURCE_KEY_MASTER;
-import static nextstep.subway.ReplicationRoutingDataSource.DATASOURCE_KEY_SLAVE;
+//import static nextstep.subway.ReplicationRoutingDataSource.DATASOURCE_KEY_MASTER;
+//import static nextstep.subway.ReplicationRoutingDataSource.DATASOURCE_KEY_SLAVE;
 
-@Profile("production")
-@Configuration
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
-@EnableTransactionManagement
-@EnableJpaRepositories(basePackages = {"nextstep.subway"})
+//@Profile("production")
+//@Configuration
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+//@EnableTransactionManagement
+//@EnableJpaRepositories(basePackages = {"nextstep.subway"})
 class DataBaseConfig {
-
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.hikari.master")
-    public DataSource masterDataSource() {
-        return DataSourceBuilder.create().type(HikariDataSource.class).build();
-    }
-
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.hikari.slave")
-    public DataSource slaveDataSource() {
-        return DataSourceBuilder.create().type(HikariDataSource.class).build();
-    }
-
-    @Bean
-    public DataSource routingDataSource(@Qualifier("masterDataSource") DataSource master,
-                                        @Qualifier("slaveDataSource") DataSource slave) {
-        ReplicationRoutingDataSource routingDataSource = new ReplicationRoutingDataSource();
-
-        HashMap<Object, Object> sources = new HashMap<>();
-        sources.put(DATASOURCE_KEY_MASTER, master);
-        sources.put(DATASOURCE_KEY_SLAVE, slave);
-
-        routingDataSource.setTargetDataSources(sources);
-        routingDataSource.setDefaultTargetDataSource(master);
-
-        return routingDataSource;
-    }
-
-    @Primary
-    @Bean
-    public DataSource dataSource(@Qualifier("routingDataSource") DataSource routingDataSource) {
-        return new LazyConnectionDataSourceProxy(routingDataSource);
-    }
+//
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource.hikari.master")
+//    public DataSource masterDataSource() {
+//        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+//    }
+//
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource.hikari.slave")
+//    public DataSource slaveDataSource() {
+//        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+//    }
+//
+//    @Bean
+//    public DataSource routingDataSource(@Qualifier("masterDataSource") DataSource master,
+//                                        @Qualifier("slaveDataSource") DataSource slave) {
+//        ReplicationRoutingDataSource routingDataSource = new ReplicationRoutingDataSource();
+//
+//        HashMap<Object, Object> sources = new HashMap<>();
+//        sources.put(DATASOURCE_KEY_MASTER, master);
+//        sources.put(DATASOURCE_KEY_SLAVE, slave);
+//
+//        routingDataSource.setTargetDataSources(sources);
+//        routingDataSource.setDefaultTargetDataSource(master);
+//
+//        return routingDataSource;
+//    }
+//
+//    @Primary
+//    @Bean
+//    public DataSource dataSource(@Qualifier("routingDataSource") DataSource routingDataSource) {
+//        return new LazyConnectionDataSourceProxy(routingDataSource);
+//    }
 }
